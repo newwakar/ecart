cbc62359b7c3a707d48dcdd1c599841a24befe47
newwakar <127464384+newwakar@users.noreply.github.com>
2023-12-26 11:57:29 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,6 @@
 const SUPABASE_URL = 'https://dmetwmpvrtkaigrmxubc.supabase.co';
 const SUPABASE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtZXR3bXB2cnRrYWlncm14dWJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDM1Njk2ODIsImV4cCI6MjAxOTE0NTY4Mn0.smS3iIzKfDRb-uyxhEaqapBiWwtJ7LTiUd9JZ2t8XkM';
 
-// Import the Supabase client library
-import { createClient } from '@supabase/supabase-js';
-
 // Connect to your Supabase project
 const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
 
