2f81b7d76c24c158f0e0cfb9a1e9195666e8949e
newwakar <127464384+newwakar@users.noreply.github.com>
2023-12-28 23:15:05 +0530

    Update initSupabase.js

--- assets/lib/initSupabase.js
+++ assets/lib/initSupabase.js
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://dmetwmpvrtkaigrmxubc.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtZXR3bXB2cnRrYWlncm14dWJjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwMzU2OTY4MiwiZXhwIjoyMDE5MTQ1NjgyfQ.Q403fw-4yeNaMeWpf_84J6QilHkYiSw6VMr8svesgqo';
-const supabase = createClient(supabaseUrl, supabseKey);
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export { supabase };
