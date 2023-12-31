9ad522c9a54ab5f6ab4cc938cdaf8b744044c54a
newwakar <127464384+newwakar@users.noreply.github.com>
2023-12-31 19:00:52 +0530

    Delete assets/lib directory

--- assets/lib/initSupabase.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://dmetwmpvrtkaigrmxubc.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtZXR3bXB2cnRrYWlncm14dWJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDM1Njk2ODIsImV4cCI6MjAxOTE0NTY4Mn0.smS3iIzKfDRb-uyxhEaqapBiWwtJ7LTiUd9JZ2t8XkM';
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export { supabase };
