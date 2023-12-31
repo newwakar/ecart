68bf486781b0bbd18d68211545a3df015ed24fbb
newwakar <127464384+newwakar@users.noreply.github.com>
2023-12-31 19:00:13 +0530

    Update store-order.js

--- assets/api/store-order.js
+++ assets/api/store-order.js
@@ -1,4 +1,8 @@
-import { supabase } from './lib/initSupabase';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://dmetwmpvrtkaigrmxubc.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtZXR3bXB2cnRrYWlncm14dWJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDM1Njk2ODIsImV4cCI6MjAxOTE0NTY4Mn0.smS3iIzKfDRb-uyxhEaqapBiWwtJ7LTiUd9JZ2t8XkM';
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 const webhook = async (req, res) => {
  if (req.method === 'POST') {
